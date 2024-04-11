b0c7c90780d2e7ff19403662ebeb469f7f063fed
Huzaifa <creativehuzaifa97@gmail.com>
2024-04-11 22:51:56 +0500

    Sorting algorithm DSA

---
+++ structureSortAlogorithm.cpp
@@ -0,0 +1,13 @@
+// C++ program to demonstrate structure sorting in C++
+#include <bits/stdc++.h>
+using namespace std;
+ 
+struct Student 
+{
+    string name; // Given
+    int math; // Marks in math (Given)
+    int phy; // Marks in Physics (Given)
+    int che; // Marks in Chemistry (Given)
+    int total; // Total marks (To be filled)
+    int rank; // Rank of student (To be filled)
+};
