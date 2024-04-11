70b3f0277ba09722fccb7cfe6ad97b61433e13af
Huzaifa <creativehuzaifa97@gmail.com>
2024-04-11 22:53:50 +0500

    Sorting algorithms DSA

--- structureSortAlogorithm.cpp
+++ structureSortAlogorithm.cpp
@@ -47,3 +47,56 @@ void computeRanks(Student a[], int n)
     for (int i = 0; i < n; i++)
         a[i].rank = i + 1;
 }
+
+// Driver code
+int main()
+{
+    int n = 5;
+ 
+    // array of structure objects
+    Student a[n];
+ 
+    // Details of Student 1
+    a[0].name = "Bryan";
+    a[0].math = 80;
+    a[0].phy = 95;
+    a[0].che = 85;
+ 
+    // Details of Student 2
+    a[1].name = "Kevin";
+    a[1].math = 95;
+    a[1].phy = 85;
+    a[1].che = 99;
+ 
+    // Details of Student 3
+    a[2].name = "Nicky";
+    a[2].math = 95;
+    a[2].phy = 85;
+    a[2].che = 80;
+ 
+    // Details of Student 4
+    a[3].name = "Steve";
+    a[3].math = 80;
+    a[3].phy = 70;
+    a[3].che = 90;
+ 
+    // Details of Student 5
+    a[4].name = "Rohan";
+    a[4].math = 80;
+    a[4].phy = 80;
+    a[4].che = 80;
+ 
+    computeRanks(a, n);
+ 
+    // Column names for displaying data
+    cout << "Rank"
+         << " "
+         << "Name"
+         << "     ";
+    cout << "Maths"
+         << " "
+         << "Physics"
+         << " "
+         << "Chemistry";
+    cout << " "
+         << "Total\n";
